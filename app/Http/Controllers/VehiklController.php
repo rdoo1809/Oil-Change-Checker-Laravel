f36789b59ec7b41a09ace4589ebb984cf13caa22
Ryan <r1d8o0o9@gmail.com>
2026-01-02 12:37:10 -0500

    move helper to model from controller

--- app/Http/Controllers/VehiklController.php
+++ app/Http/Controllers/VehiklController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\VehiklRequest;
 use App\Models\Vehikl;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class VehiklController extends Controller
 {
@@ -13,26 +12,18 @@ class VehiklController extends Controller
     {
         $vehikl = Vehikl::create($request->validated());
         return redirect()->route('vehikl.show', $vehikl);
-
-//        return response()->json([
-//            'id' => $vehikl->id,
-//            'current_odometer' => $vehikl->current_odometer,
-//            'previous_odometer' => $vehikl->previous_odometer,
-//            'previous_oil_change_date' => $vehikl->previous_oil_change_date,
-//            'due_for_oil_change' => $this->isDue($vehikl),
-//            'message' => $this->isDue($vehikl) ? 'Oil change is due.' : 'Oil change is not due.',
-//        ]);
     }
 
     public function show(Vehikl $vehikl)
     {
-        $due = $this->isDue($vehikl);
+        $due = $vehikl->isDue();
+
         return view('show', [
             'vehikl' => $vehikl,
             'due' => $due,
             'message' => $due ? 'Oil change is due.' : 'Oil change is not due.',
         ]);
-
+    }
 //        return response()->json([
 //            'id' => $vehikl->id,
 //            'current_odometer' => $vehikl->current_odometer,
@@ -41,16 +32,4 @@ class VehiklController extends Controller
 //            'due_for_oil_change' => $due,
 //            'message' => $due ? 'Oil change is due.' : 'Oil change is not due.',
 //        ]);
-    }
-
-    protected function isDue(Vehikl $vehikl): bool
-    {
-        $distanceDue = ($vehikl->current_odometer - $vehikl->previous_odometer) > 5000;
-
-        $dateDue = Carbon::parse($vehikl->previous_oil_change_date)
-            ->addMonths(6)
-            ->isPast();
-
-        return $distanceDue || $dateDue;
-    }
 }
--- app/Models/Vehikl.php
+++ app/Models/Vehikl.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Vehikl extends Model
@@ -11,4 +12,12 @@ class Vehikl extends Model
         'previous_odometer',
         'previous_oil_change_date',
     ];
+
+    public function isDue(): bool
+    {
+        $distanceDue = ($this->current_odometer - $this->previous_odometer) > 5000;
+        $dateDue = Carbon::parse($this->previous_oil_change_date)->addMonths(6)->isPast();
+
+        return $distanceDue || $dateDue;
+    }
 }
