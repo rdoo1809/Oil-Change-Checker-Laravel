bb2d7d1cf20ef8c06b187ecc00974a811556fa9d
Ryan <r1d8o0o9@gmail.com>
2026-01-02 11:23:09 -0500

    create mrc for Vehikl model

---
+++ app/Http/Controllers/VehiklController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehikl;
+use Illuminate\Http\Request;
+
+class VehiklController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vehikl $vehikl)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vehikl $vehikl)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vehikl $vehikl)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vehikl $vehikl)
+    {
+        //
+    }
+}
