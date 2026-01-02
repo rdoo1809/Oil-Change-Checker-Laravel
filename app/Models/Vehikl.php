14ffeb5d9b211e97f570a44be17dc3add6e5bdbb
Ryan <r1d8o0o9@gmail.com>
2026-01-02 11:45:34 -0500

    define required routes

--- app/Models/Vehikl.php
+++ app/Models/Vehikl.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehikl extends Model
 {
-    //
+    protected $fillable = [
+        'current_odometer',
+        'previous_odometer',
+        'previous_oil_change_date',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\VehiklController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::controller(VehiklController::class)->group(function () {
+    Route::post('/check', 'store')->name('vehikl.store');
+    Route::get('/result/{vehikl}', 'show')->name('vehikl.show');
 });
